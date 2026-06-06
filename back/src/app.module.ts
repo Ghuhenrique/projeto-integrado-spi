34bf23ec6a6812b656ca4a4a9509ba2c5ddaabc9
luan-cavalcante-dev <luansantos1802@gmail.com>
2026-06-05 23:44:06 -0300

    feat: configura integração inicial com Neo4j

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuarioModule } from './usuario/usuario.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsuarioModule],
+  imports: [UsuarioModule, ConfigModule.forRoot( {isGlobal: true,})],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
