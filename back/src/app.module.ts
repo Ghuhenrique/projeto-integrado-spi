f5d582f595d20848b301999d4be7c9a56aecf7f8
luan-cavalcante-dev <luansantos1802@gmail.com>
2026-06-08 19:35:37 -0300

    feat: implementa middleware de log para requisições

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,18 +1,43 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  MiddlewareConsumer,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { UsuarioModule } from './usuario/usuario.module';
-import { ConfigModule } from '@nestjs/config';
 import { Neo4jModule } from './neo4j/neo4j.module';
+
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [
-    UsuarioModule, 
-    ConfigModule.forRoot( {isGlobal: true,}),
-    Neo4jModule
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    UsuarioModule,
+    Neo4jModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
 
+  configure(
+    consumer: MiddlewareConsumer,
+  ) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({
+        path: '*',
+        method: RequestMethod.ALL,
+      });
+  }
+
+}
