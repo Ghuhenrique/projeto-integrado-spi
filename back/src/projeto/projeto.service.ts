233198b94d9e28a2049f91f47fdc9d05afa4f440
= <=>
2026-06-09 20:50:58 -0300

    add findall sample

--- back/src/projeto/projeto.service.ts
+++ back/src/projeto/projeto.service.ts
@@ -1,15 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProjetoDto } from './dto/create-projeto.dto';
 import { UpdateProjetoDto } from './dto/update-projeto.dto';
+import { Neo4jService } from 'src/neo4j/neo4j.service';
 
 @Injectable()
 export class ProjetoService {
+  constructor(private readonly neo4jService: Neo4jService) { }
+  
   create(createProjetoDto: CreateProjetoDto) {
     return 'This action adds a new projeto';
   }
 
   findAll() {
-    return `This action returns all projeto`;
+    const session = this.neo4jService.getReadSession();
+    try {
+      const result = await session.run('MATCH (n:Professor) RETURN n.id AS id, n.nome AS nome, n.email as email LIMIT 25');
+      const alunos = result.records.map(r => {
+        console.log(r);
+        return {
+          email:r.get('email'),
+          nome: r.get('nome'),
+          id: r.get('id')
+        }
+      });
+      return alunos;
+    } catch (error) {
+      console.error('Erro Neo4J: ', error);
+      throw error;
+    } finally {
+      await session.close();
+    }
   }
 
   findOne(id: number) {
