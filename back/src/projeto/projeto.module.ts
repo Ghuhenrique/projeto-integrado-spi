e503abe8b8164d34ba84a23b720a15e1e1d2a978
= <=>
2026-06-09 21:00:33 -0300

    fix: add neo4jmodule

--- back/src/projeto/projeto.module.ts
+++ back/src/projeto/projeto.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProjetoService } from './projeto.service';
 import { ProjetoController } from './projeto.controller';
+import { Neo4jModule } from 'src/neo4j/neo4j.module';
 
 @Module({
   controllers: [ProjetoController],
   providers: [ProjetoService],
+  imports:[Neo4jModule]
 })
 export class ProjetoModule {}
